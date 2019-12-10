d98588eebbc0304c066ef67b8da5621a76ca2ea8
Tu Vo <tuvo1106@gmail.com>
2019-12-09 20:08:34 -0800

    Add solution for 80_remove_dups in Python and C++

---
+++ 80_remove_dups_sorted_array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Given a sorted array nums, remove the duplicates in-place such that 
+ * duplicates appeared at most twice and return the new length.
+ * Do not allocate extra space for another array, you must do this by
+ * modifying the input array in-place with O(1) extra memory. 
+ * 
+ */
+int removeDuplicates(vector<int> &nums)
+{
+    int i = 0;
+    for (auto e : nums)
+        if (i < 2 || nums[i - 2] < e)
+            nums[i++] = e;
+    return i;
+}
+
+int main()
+{
+    vector<int> arr{1, 1, 1, 2, 2, 3};
+    cout << removeDuplicates(arr) << endl;
+    return 0;
+}
