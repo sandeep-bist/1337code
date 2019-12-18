c0ebe760de91169c1ff511f0f7eba1fc1854877f
Tu Vo <tuvo1106@gmail.com>
2019-12-17 21:47:08 -0800

    Add solution for 13_roman in Python

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,20 @@
+def roman_to_int(s: str) -> int:
+    """
+    Given a roman numeral, convert it to an integer. Input is guaranteed to be
+    within the range from 1 to 3999.
+    """
+    hash_map = {"I": 1, "V": 5, "X": 10,
+                "L": 50, "C": 100, "D": 500, "M": 1000}
+    sum = prev = 0
+    for i in s[::-1]:
+        curr = hash_map[i]
+        if curr < prev:
+            sum -= curr
+        else:
+            sum += curr
+        prev = curr
+    return sum
+
+
+print(roman_to_int("MCMXCIV"))  # 1994
+print(roman_to_int("IX"))  # 9
