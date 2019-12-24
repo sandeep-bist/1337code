9b331a0dc07e30ee322165e0643db17f7f3fe5af
Tu Vo <tuvo1106@gmail.com>
2019-12-24 14:38:03 -0800

    Add solution for 208_implement_trie in C++

---
+++ 208_implement_trie.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class TrieNode
+{
+public:
+    unordered_map<char, TrieNode *> children;
+    bool is_word = false;
+};
+
+class Trie
+{
+public:
+    void insert(string word);
+    bool search(string word);
+    bool starts_with(string prefix);
+
+private:
+    TrieNode root;
+};
+
+/**
+ * Inserts a word into the trie.
+ */
+void Trie::insert(string word)
+{
+    TrieNode *curr = &root;
+    for (auto c : word)
+    {
+        if (!curr->children.count(c))
+            curr->children[c] = new TrieNode();
+        curr = curr->children[c];
+    }
+    curr->is_word = true;
+}
+
+/**
+ * Determines if word is in the trie.
+ */
+bool Trie::search(string word)
+{
+    TrieNode *curr = &root;
+    for (auto c : word)
+    {
+        if (!curr->children.count(c))
+            return false;
+        curr = curr->children[c];
+    }
+    return curr->is_word;
+}
+
+/**
+ * Determines if there is any word in the trie that starts with the
+ * given prefix.
+ */
+bool Trie::starts_with(string prefix)
+{
+    TrieNode *curr = &root;
+    for (auto c : prefix)
+    {
+        if (!curr->children.count(c))
+            return false;
+        curr = curr->children[c];
+    }
+    return true;
+}
+
+int main()
+{
+    Trie t;
+    t.insert("hello");
+    t.insert("hey");
+    cout << t.search("hello") << endl;   // 1
+    cout << t.search("hey") << endl;     // 1
+    cout << t.search("he") << endl;      // 0
+    cout << t.starts_with("he") << endl; // 1
+    return 0;
+}
