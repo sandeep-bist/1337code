fcbebd2998c381e9909993e5259e4cedc6620396
Tu Vo <tuvo1106@gmail.com>
2020-03-07 12:18:44 -0800

    Update README.md

--- 0072_edit_distance/min_distance.py
+++ 0072_edit_distance/min_distance.py
@@ -1,21 +1,15 @@
-def min_distance(word1, word2):
+def min_distance(word1: str, word2: str) -> int:
     """
-    Given two words word1 and word2, find the minimum number of operations
-    required to convert word1 to word2.
-    You have the following 3 operations permitted on a word:
-    Insert a character
-    Delete a character
-    Replace a character
+    Time: O(m * n)
+    Space: O(m * n)
     """
     m = len(word1)
     n = len(word2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
-
     for i in range(m + 1):
         dp[i][0] = i
     for j in range(n + 1):
         dp[0][j] = j
-
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if word1[i - 1] == word2[j - 1]:
@@ -24,6 +18,3 @@ def min_distance(word1, word2):
                 dp[i][j] = 1 + min(dp[i - 1][j],
                                    dp[i][j - 1], dp[i - 1][j - 1])
     return dp[-1][-1]
-
-
-print(min_distance("dog", "hop"))  # 2
