06e996fc1dd2f529cf5d3142ccd9707fd97e2c88
Tu Vo <tuvo1106@gmail.com>
2020-03-13 09:38:01 -0700

    Update typing

--- 0937_reorder_data_in_log_files/reorder_log_files.py
+++ 0937_reorder_data_in_log_files/reorder_log_files.py
@@ -6,7 +6,7 @@ def reorder_log_files(logs: List[str]) -> List[str]:
     Time: O(n * log(n))
     Space: O(n)
     """
-    def custom(log: str) -> Tuple(int, str):
+    def custom(log: str) -> Tuple[int, str]:
         id_, rest = log.split(" ", 1)
         return (0, rest, id_) if rest[0].isalpha() else (1,)
     return sorted(logs, key=custom)
