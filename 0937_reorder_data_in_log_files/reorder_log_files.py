f0bb5b4fb3ab3af65b0aee5cfb557c083e0f46b8
Tu Vo <tuvo1106@gmail.com>
2020-03-13 09:35:18 -0700

    Add solution for 937_reorder_log in Python

---
+++ 0937_reorder_data_in_log_files/reorder_log_files.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+
+
+def reorder_log_files(logs: List[str]) -> List[str]:
+    """
+    Time: O(n * log(n))
+    Space: O(n)
+    """
+    def custom(log: str) -> Tuple(int, str):
+        id_, rest = log.split(" ", 1)
+        return (0, rest, id_) if rest[0].isalpha() else (1,)
+    return sorted(logs, key=custom)
