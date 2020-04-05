99174263071e419b535f11fa5b36dcb5731d97e9
Tu Vo <tuvo1106@gmail.com>
2020-04-05 14:32:11 -0700

    Add solution for 232_implement_queue in Python

---
+++ 0232_implement_queue/queue.py
@@ -0,0 +1,44 @@
+class Queue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        if not self.stack2:
+            self.shift()
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if self.stack2:
+            return self.stack2[-1]
+        self.shift()
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not self.stack1 and not self.stack2
+
+    def shift(self) -> None:
+        """
+        Shifts all elements in stack1 to stack2.
+        """
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
