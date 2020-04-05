16dc42e676c1338be127a3f3abaa77c17985028e
Tu Vo <tuvo1106@gmail.com>
2020-04-05 07:16:53 -0700

    Add solutions for 122 in Python, C++, JS and Go

--- 0122_best_time_to_buy_and_sell_stock_II/max_profit.js
+++ 0122_best_time_to_buy_and_sell_stock_II/max_profit.js
@@ -1,10 +1,8 @@
 /**
  * Time:    O(n)
  * Space:   O(1)
- * @param   {number[]} prices
- * @return  {number}
  */
-const maxProfit = prices => {
+const maxProfit = (prices) => {
   return prices.reduce((max, el, i) => {
     if (el > prices[i - 1]) max += el - prices[i - 1]
     return max
