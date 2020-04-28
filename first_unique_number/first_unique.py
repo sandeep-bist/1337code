d885d6b4bc03569c55c1f8070c54871d6e499fec
Tu Vo <tuvo1106@gmail.com>
2020-04-28 08:56:39 -0700

    Edit documentation

--- first_unique_number/first_unique.py
+++ first_unique_number/first_unique.py
@@ -3,19 +3,27 @@ from typing import List
 
 
 class FirstUnique:
-
     def __init__(self, nums: List[int]):
+        """
+        Space: O(n)
+        """
         self.set = set()
         self.od = OrderedDict()
         for n in nums:
             self.add(n)
 
     def show_first_unique(self) -> int:
+        """
+        Time: O(1)
+        """
         for k in self.od.keys():
             return k
         return -1
 
     def add(self, value: int) -> None:
+        """
+        Time: O(1)
+        """
         if value not in self.set:
             self.set.add(value)
             self.od[value] = 1
