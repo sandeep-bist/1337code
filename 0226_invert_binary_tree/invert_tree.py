0e1a8ce04b18697ac0355b54953bc2a06969c0b0
Tu Vo <tuvo1106@gmail.com>
2020-06-01 08:13:07 -0700

    Add solutions for 226_invert_tree in Python and Go

---
+++ 0226_invert_binary_tree/invert_tree.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def invert_tree(root: TreeNode) -> TreeNode:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    if root:
+        root.left, root.right = invert_tree(
+            root.right), invert_tree(root.left)
+    return root
