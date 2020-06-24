eacacdef11fd173f2a4ed877a7df56dd0e511400
Tu Vo <tuvo@macbook-pro.attlocal.net>
2020-06-24 08:59:44 -0700

    Add solutions for 96_count_bsts in Python and Go

---
+++ 0096_unique_bst/count_trees.go
@@ -0,0 +1,21 @@
+package main
+
+/**
+ * Time: O(n**2)
+ * Space: O(n)
+ */
+func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	for i := 2; i < n+1; i++ {
+		for j := 1; j < i+1; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+func main() {
+
+}
---
+++ 0096_unique_bst/count_trees.py
@@ -0,0 +1,11 @@
+def num_trees(n: int) -> int:
+    """
+    Time: O(n**2)
+    Space: O(n)
+    """
+    dp = [0]*(n + 1)
+    dp[0], dp[1] = 1, 1
+    for i in range(2, n + 1):
+        for j in range(1, i + 1):
+            dp[i] += dp[j - 1] * dp[i - j]
+    return dp[n]
