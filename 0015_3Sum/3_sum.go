740106ee005ae5984810902a0edb3e7da515f6e5
Tu Vo <tuvo1106@gmail.com>
2020-07-08 09:22:57 -0700

    Add solution for 15_3Sum in Go

---
+++ 0015_3Sum/3_sum.go
@@ -0,0 +1,43 @@
+package main
+
+import "sort"
+
+// Time: O(n * log(n)) * n
+// Space: O(n)
+func threeSum(nums []int) [][]int {
+	res := make([][]int, 0)
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum > 0 {
+				r--
+			} else if sum < 0 {
+				l++
+			} else {
+				tmp := []int{nums[i], nums[l], nums[r]}
+				res = append(res, tmp)
+				for l < r && nums[l] == nums[l+1] {
+					l++
+				}
+				for l < r && nums[r] == nums[r-1] {
+					r--
+				}
+				l++
+				r--
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+
+}
