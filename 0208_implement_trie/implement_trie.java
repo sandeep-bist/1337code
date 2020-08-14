50db0bae3a3e5aba3583eeb575830d720ee5bc89
Tu Vo <tuvo1106@gmail.com>
2020-08-14 12:12:19 -0700

    Add solution for 208_implement_trie in Java

---
+++ 0208_implement_trie/implement_trie.java
@@ -0,0 +1,57 @@
+class TrieNode {
+    public char val;
+    public boolean isWord;
+    public TrieNode[] children = new TrieNode[26];
+
+    TrieNode() {
+    };
+}
+
+class Trie {
+
+    /** Initialize your data structure here. */
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.children[c - 'a'] == null) {
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isWord = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isWord;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+}
