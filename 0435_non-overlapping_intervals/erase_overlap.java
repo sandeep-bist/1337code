bf7549bc9f31b565e37207879551a2d93c3545e6
Tu Vo <tuvo1106@gmail.com>
2020-08-15 09:33:54 -0700

    Add solutions for 435_non-overlapping in Python, Java and JS

---
+++ 0435_non-overlapping_intervals/erase_overlap.java
@@ -0,0 +1,18 @@
+// Time: O(n * log(n))
+// Space: O(1)
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0)
+            return 0;
+        Arrays.sort(intervals, (x, y) -> (x[1] - y[1]));
+        int end = intervals[0][1];
+        int count = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= end) {
+                end = intervals[i][1];
+                count++;
+            }
+        }
+        return intervals.length - count;
+    }
+}
