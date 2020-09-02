b50e8823dfe67660d1ee6be3b946a40ab9cb1a40
Tu Vo <tuvo1106@gmail.com>
2020-09-02 09:50:15 -0700

    Add solutions for 220_contains_duplicate in Python and Java

---
+++ 0220_contains_duplicate_iii/contains_nearby.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+def contains_nearby_almost_duplicate(nums: List[int], k: int, t: int) -> bool:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    if t < 0:
+        return False
+    n = len(nums)
+    d = {}
+    w = t + 1
+    for i in range(n):
+        m = nums[i] // w
+        if m in d:
+            return True
+        if m - 1 in d and abs(nums[i] - d[m - 1]) < w:
+            return True
+        if m + 1 in d and abs(nums[i] - d[m + 1]) < w:
+            return True
+        d[m] = nums[i]
+        if i >= k:
+            del d[nums[i - k] // w]
+    return False
