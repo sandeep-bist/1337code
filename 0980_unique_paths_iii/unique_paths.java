5ba2324b1599b057b50c1295da3a06fc92af068c
Tu Vo <tuvo1106@gmail.com>
2020-09-21 09:27:20 -0700

    Add solution for 980_unique_paths_iii in Java

---
+++ 0980_unique_paths_iii/unique_paths.java
@@ -0,0 +1,50 @@
+class Solution {
+    int rows, cols;
+    int[][] grid;
+    int path_count;
+
+    protected void backtrack(int row, int col, int remain) {
+        if (this.grid[row][col] == 2 && remain == 1) {
+            this.path_count += 1;
+            return;
+        }
+        int temp = grid[row][col];
+        grid[row][col] = -4;
+        remain -= 1;
+        int[] row_offsets = { 0, 0, 1, -1 };
+        int[] col_offsets = { 1, -1, 0, 0 };
+        for (int i = 0; i < 4; ++i) {
+            int next_row = row + row_offsets[i];
+            int next_col = col + col_offsets[i];
+            if (0 > next_row || next_row >= this.rows || 0 > next_col || next_col >= this.cols)
+                continue;
+            if (grid[next_row][next_col] < 0)
+                continue;
+            backtrack(next_row, next_col, remain);
+        }
+        grid[row][col] = temp;
+    }
+
+    // Time: O(3**n)
+    // Space: O(n)
+    public int uniquePathsIII(int[][] grid) {
+        int non_obstacles = 0, start_row = 0, start_col = 0;
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        for (int row = 0; row < rows; ++row)
+            for (int col = 0; col < cols; ++col) {
+                int cell = grid[row][col];
+                if (cell >= 0)
+                    non_obstacles += 1;
+                if (cell == 1) {
+                    start_row = row;
+                    start_col = col;
+                }
+            }
+        this.path_count = 0;
+        this.grid = grid;
+        backtrack(start_row, start_col, non_obstacles);
+        return this.path_count;
+    }
+
+}
