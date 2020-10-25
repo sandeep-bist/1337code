dd50f9c252d84397a3eba7b148b208274b1598d0
Tu Vo <tuvo1106@gmail.com>
2020-10-25 09:45:55 -0700

    Add solutions for 1510_stone_game in Python and Java

---
+++ 1510_stone_game_iv/winner_square_game.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Time: O(n * sqrt(n))
+    // Space: O(n)
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int k = 1; k * k <= i; k++) {
+                if (dp[i - k * k] == false) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
---
+++ 1510_stone_game_iv/winner_square_game.py
@@ -0,0 +1,12 @@
+def winnerSquareGame(n: int) -> bool:
+    """
+    Time: O(n * sqrt(n))
+    Space: O(n)
+    """
+    dp = [False]*(n+1)
+    for i in range(1, n+1):
+        for k in range(1, int(i**0.5)+1):
+            if dp[i-k*k] == False:
+                dp[i] = True
+                break
+    return dp[n]
