4d2529765d62f2f7e30627468ad6c6652acee353
Tu Vo <tuvo1106@gmail.com>
2020-12-23 09:05:03 -0800

    Add solutions for 556_next_greater in Python and Java

---
+++ 0556_next_greater_element_lll/next_greater_element.py
@@ -0,0 +1,23 @@
+def nextGreaterElement(n: int) -> int:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    s = [int(x) for x in str(n)]
+    i = len(s) - 1
+
+    while i - 1 >=0 and s[i] <= s[i-1]:
+        i -= 1
+        
+    if i == 0:
+        return -1
+    
+    j = i
+    while j + 1 < len(s) and s[j + 1] > s[i - 1]:
+        j += 1
+    
+    s[i - 1], s[j] = s[j], s[i - 1]
+    s[i:] = reversed(s[i:])
+    res = int("".join([str(x) for x in s]))
+    
+    return res if res <= ((1 << 31) - 1) else -1
