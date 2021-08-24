c205f6d1ed62c0303a656c69b4c26d7d226e43cb
Tu Vo <tuvo1106@gmail.com>
2021-08-24 16:06:06 -0700

    Add solutions in Java

---
+++ 0019_remove_nth_node_from_end_of_list/remove_nth_node.java
@@ -0,0 +1,21 @@
+public class Solution {
+    // Time: O(n)
+    // Space: O(1)
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (n != 0) {
+            fast = fast.next;
+            n--;
+        }
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
