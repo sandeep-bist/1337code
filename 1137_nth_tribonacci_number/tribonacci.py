ab6994f419c5197134850ddc9aaf7b002fe77825
Tu Vo <tuvo1106@gmail.com>
2021-09-24 13:05:43 -0700

    Add solutions for 1137_nth_tribonacci in Python and Java

---
+++ 1137_nth_tribonacci_number/tribonacci.py
@@ -0,0 +1,14 @@
+def tribonacci(n: int) -> int:
+    """
+    Time: O(n)
+    Space: O(n)
+    """
+    dp = [None for _ in range(n + 1)]
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 1
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+    return dp[n]
+            
+            
